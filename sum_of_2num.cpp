5cbd074e977e286f4103552dd0f9bc33f7d731ec
Yash Surjuse <yashrsurjused@gmail.com>
2026-06-10 22:28:54 +0530

    Create sum_of_2num.cpp

---
+++ sum_of_2num.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+
+    cout<<"Enter a : ";
+    cin>>a;
+
+    cout<<"Enter b : ";
+    cin>>b;
+
+    int sum = a + b;
+    int prod = a * b;
+    int diff = a - b;
+    cout<<"Sum = "<<sum<<endl;
+    cout<<"Prod = "<<prod<<endl;
+    cout<<"Diff = "<<diff<<endl;
+    return 0;
+}
