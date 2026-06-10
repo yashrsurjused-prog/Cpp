95d7f05af2dc6ea39c9300b6505b74a7c7841b32
Yash Surjuse <yashrsurjused@gmail.com>
2026-06-10 22:56:49 +0530

    Create explicit.cpp

---
+++ explicit.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float PI = 3.14;
+    cout << (int)(PI) << endl;
+    cout << ((float)10/3) << endl;
+    cout << (char)('A' + 2) << endl;
+    
+    return 0;
+}
