cb3cedf6c2597e0e2450f57ec6027e0f2330c723
Yash Surjuse <yashrsurjused@gmail.com>
2026-06-11 10:27:58 +0530

    Create assignment_op.cpp

---
+++ assignment_op.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 20;
+
+    a += 5;
+    cout << a << endl;
+
+    a -= 5;
+    cout << a << endl;
+
+    a *= 5;
+    cout << a << endl;
+    
+    a /= 5;
+    cout << a << endl;
+
+    return 0;
+}
