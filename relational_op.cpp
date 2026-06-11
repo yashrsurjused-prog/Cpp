85dec17275a8b0c4320531bc3149169d929f6ee7
Yash Surjuse <yashrsurjused@gmail.com>
2026-06-11 10:34:58 +0530

    Create relational_op.cpp

---
+++ relational_op.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int a = 4;
+int b = 8;
+cout << (a == b) << endl;
+cout << ( a != b) << endl;
+cout << (a < b) << endl;
+cout << (a > b) << endl;
+cout << (a <= b) << endl;
+cout << (a >= b) << endl;
+
+return 0;
+}
