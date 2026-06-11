ded998b17b77e5be8079e7b010643adc4ea996d1
Yash Surjuse <yashrsurjused@gmail.com>
2026-06-11 09:49:04 +0530

    Create explicit_PQ2.cpp

---
+++ explicit_PQ2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //cout << ((bool)3 +2) << endl;
+    cout << (23.5 + 2 + 'A') << endl;
+    return 0;
+}
+
+Predict output of cout << (23.5 + 2 + 'A');
