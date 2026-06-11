a89a5dcb4cf75b8a314830c956e790ec9be7051a
Yash Surjuse <yashrsurjused@gmail.com>
2026-06-11 11:46:56 +0530

    Create area_of_circle.cpp

---
+++ area_of_circle.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float r;
+    cout << "Enter the radius of circle : ";
+    cin >> r;
+    float area = 3.14 * r * r;
+    cout << "Area of circle is : " << area << endl;
+    return 0;
+}
